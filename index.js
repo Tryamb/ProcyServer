91f67988a276c8436e4a9afc0c22d1d1566403d7
Tryamb <tryambsachan69@gmail.com>
2024-12-18 12:32:11 +0530

    setting up server

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-    origin: 'https://your-frontend.vercel.app',
+    origin: process.env.Allowed_Domain,
     methods: ['GET'],
   };
 
@@ -19,23 +19,29 @@ app.use('/api', layer);
 
 app.get('/api', async (req, res) => {
     const { exam, practiceSet } = req.query;
-    const appscriptUrl = "https://script.google.com/macros/s/AKfycbwHw7S89nHJd7wakH6s-hCBj09SwhF_jAT8tK9aLxZtc4YXT5A8Um1f378J3RgIQ9BHOg/exec";
+    const appscriptUrl = process.env.BASE_URL;
   
-    try {
-        const response = await axios.get(appscriptUrl, {
-            params: {
-                exam: exam,
-                practiceSet: practiceSet
-            }
-        });
+    if(!exam || !practiceSet){
+        res.status(400).json();
+    }
+    else{
+        try {
+            const response = await axios.get(appscriptUrl, {
+                params: {
+                    exam: exam,
+                    practiceSet: practiceSet
+                }
+            });
 
         res.status(200).json(response.data);
 
     } catch (error) {
         console.error('Error fetching data from Apps Script:', error);
         res.status(500).json({ error: 'Failed to fetch data from Apps Script' });
+        }
     }
 });
+
 
 app.listen(5000, () => {
     console.log(`Server running on port 5000`);
